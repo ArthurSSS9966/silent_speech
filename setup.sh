e93029cbb0e55c7f2a8d17693af42de47a180fa0
arthu <arthurshao96@gmail.com>
2024-09-03 17:43:53 -0700

    Test Speed Issue

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 source venv/bin/activate
 
-# Step 1: Install libsndfile using conda
-conda env update --file environment.yml --prune
-
-# Step 2: Install required Python packages using pip
-pip install jiwer torchaudio matplotlib scipy soundfile absl-py librosa numba unidecode praat-textgrids g2p_en einops opt_einsum hydra-core pytorch_lightning "neptune-client==0.16.18"
-
-# Install PyTorch
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+# Step 1: Install environment using conda
+conda env update --file VMenvironment.yml --prune
+#
+## Step 2: Install required Python packages using pip
+#pip install jiwer torchaudio matplotlib scipy soundfile absl-py librosa numba unidecode praat-textgrids g2p_en einops opt_einsum hydra-core pytorch_lightning "neptune-client==0.16.18"
+#
+## Install PyTorch
+#pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 
 # Git Clone the EMG alignments file
 git clone https://github.com/dgaddy/silent_speech_alignments.git
